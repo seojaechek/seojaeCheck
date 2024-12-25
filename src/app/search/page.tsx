b2601175073b00726690cdd8f5e8d61390319f51
Choongman Kim <cm730712@gmail.com>
2024-12-26 08:58:25 +0900

    Design: 검색 페이지 검색 목록 UI 수정정 - mockData를 사용해서 레이아웃 수정 - 썸네일 이미지 사이즈 수정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,143 +1,53 @@
 import Image from "next/image";
-import SearchBar from "./_components/SearchBar";
-import bookCover from "/public/SampleCover.jpeg";
 import Bookmark from "./_components/Bookmark";
+import { storedBook } from "@/mock/bookshelf";
+import SearchBar from "./_components/SearchBar";
 
 export default function Search() {
   return (
-    <section className="flexCenter mt-20 flex-col font-main">
+    <section className="flexCenter mb-10 mt-20 flex-col font-main">
       <SearchBar />
       <ul className="flex flex-col items-center gap-5">
-        <li className="searchList">
-          <article className="flex">
-            <Image
-              src={bookCover}
-              width={190}
-              height={230}
-              alt="북커버"
-              className="mx-8 my-3 shadow-xl"
-            />
-            <div className="border-border border"></div>
-            <div className="mx-8 my-4 flex w-7/12 flex-col gap-2">
-              <h2 className="font-styled text-3xl font-extrabold text-font-textPrimary">
-                Why We Sleep
-              </h2>
-              <p className="flex-grow font-medium text-font-textSecondary">
-                인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기
-                위해서는 ‘용기’가 필요하다고 말한 철학자가 있다. 바로 프로이트,
-                융과 함께 ‘심리학의 3대 거장’으로 일컬어지고 있는 알프레드
-                아들러다
-              </p>
-              <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
-                <div className="flex">
-                  <dt className="searchDetail">작가</dt>
-                  <dd className="font-semibold">360 pages, Kindle Edition</dd>
+        {storedBook.map((el, index) => {
+          return (
+            <li key={index} className="searchList">
+              <article className="relative flex justify-center">
+                <picture className="flexCenter relative mx-8 my-3 w-48 shadow-md">
+                  <Image src={el.thumbnail} fill alt="북커버" />
+                </picture>
+
+                <Bookmark />
+                <div className="border border-border" />
+                <div className="mx-8 my-4 flex w-8/12 flex-col gap-2">
+                  <h2 className="font-styled text-2xl font-extrabold text-font-textPrimary">
+                    {el.title}
+                  </h2>
+                  <p className="flex-grow text-sm font-medium text-font-textSecondary">
+                    {el.contents}
+                  </p>
+                  <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
+                    <div className="flex">
+                      <dt className="searchDetail">작가</dt>
+                      <dd className="font-semibold">{el.authors}</dd>
+                    </div>
+                    <div className="flex">
+                      <dt className="searchDetail">번역</dt>
+                      <dd className="font-semibold">{el.translators}</dd>
+                    </div>
+                    <div className="flex">
+                      <dt className="searchDetail">출판사</dt>
+                      <dd className="font-semibold">{el.publisher}</dd>
+                    </div>
+                    <div className="flex">
+                      <dt className="searchDetail">출판 날짜</dt>
+                      <dd className="font-semibold">{el.datetime}</dd>
+                    </div>
+                  </dl>
                 </div>
-                <div className="flex">
-                  <dt className="searchDetail">번역</dt>
-                  <dd className="font-semibold">
-                    First published September 28, 2017
-                  </dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">출판사</dt>
-                  <dd className="font-semibold">English</dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">출판 날짜</dt>
-                  <dd className="font-semibold">August, 2024</dd>
-                </div>
-              </dl>
-            </div>
-          </article>
-        </li>
-        <li className="searchList">
-          <article className="flex">
-            <Image
-              src={bookCover}
-              width={190}
-              height={230}
-              alt="북커버"
-              className="mx-8 my-3 shadow-xl"
-            />
-            <div className="border-border border"></div>
-            <div className="mx-8 my-4 flex w-7/12 flex-col gap-2">
-              <h2 className="font-styled text-3xl font-extrabold text-font-textPrimary">
-                Why We Sleep
-              </h2>
-              <p className="flex-grow font-medium text-font-textSecondary">
-                인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기
-                위해서는 ‘용기’가 필요하다고 말한 철학자가 있다. 바로 프로이트,
-                융과 함께 ‘심리학의 3대 거장’으로 일컬어지고 있는 알프레드
-                아들러다
-              </p>
-              <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
-                <div className="flex">
-                  <dt className="searchDetail">작가</dt>
-                  <dd className="font-semibold">360 pages, Kindle Edition</dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">번역</dt>
-                  <dd className="font-semibold">
-                    First published September 28, 2017
-                  </dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">출판사</dt>
-                  <dd className="font-semibold">English</dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">출판 날짜</dt>
-                  <dd className="font-semibold">August, 2024</dd>
-                </div>
-              </dl>
-            </div>
-          </article>
-        </li>
-        <li className="searchList">
-          <article className="relative flex">
-            <Image
-              src={bookCover}
-              width={190}
-              height={230}
-              alt="북커버"
-              className="mx-8 my-3 shadow-xl"
-            />
-            <Bookmark />
-            <div className="border-border border" />
-            <div className="mx-8 my-4 flex w-7/12 flex-col gap-2">
-              <h2 className="font-styled text-3xl font-extrabold text-font-textPrimary">
-                Why We Sleep
-              </h2>
-              <p className="flex-grow font-medium text-font-textSecondary">
-                인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기
-                위해서는 ‘용기’가 필요하다고 말한 철학자가 있다. 바로 프로이트,
-                융과 함께 ‘심리학의 3대 거장’으로 일컬어지고 있는 알프레드
-                아들러다
-              </p>
-              <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
-                <div className="flex">
-                  <dt className="searchDetail">작가</dt>
-                  <dd className="font-semibold">360 pages, Kindle Edition</dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">번역</dt>
-                  <dd className="font-semibold">
-                    First published September 28, 2017
-                  </dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">출판사</dt>
-                  <dd className="font-semibold">English</dd>
-                </div>
-                <div className="flex">
-                  <dt className="searchDetail">출판 날짜</dt>
-                  <dd className="font-semibold">August, 2024</dd>
-                </div>
-              </dl>
-            </div>
-          </article>
-        </li>
+              </article>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
