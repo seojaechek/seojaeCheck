3ec0f3eceb6d07e9dec53b2f5df7d08a90c0a6e7
Choongman Kim <cm730712@gmail.com>
2025-01-03 00:19:42 +0900

    Feat: 검색 API 통신 및 데이터 패칭 구현 - kakao 검색 api 통신용 코드 구현 - 검색어를 통해 받은 데이터 패칭 hook 구현

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 export default function useDebounce<T>(value: T, delay: number): T {
---
+++ src/hooks/useSearchResult.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import useDebounce from "./useDebounce";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchResult } from "@/stores/searchResult";
+import { getBookSearch, BookResponse } from "@/libs/apis/searchApi";
+
+export function useSearchResultQuery() {
+  const { query } = useSearchResult();
+  const debouncedQuery = useDebounce(query, 300);
+
+  const { data, isLoading, isError, error, refetch, isFetching } = useQuery<
+    BookResponse,
+    Error
+  >({
+    queryKey: ["searchBooks", debouncedQuery],
+    queryFn: () => getBookSearch(debouncedQuery, 1, 10, "accuracy"),
+    enabled: debouncedQuery.length > 0,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  return {
+    data,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+    refetch,
+  };
+}
