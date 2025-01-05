88f8c9f950fc581ecfcefd36397b411f496a177d
Choongman Kim <cm730712@gmail.com>
2025-01-05 23:55:45 +0900

    Design: 검색 페이지 css 수정 - 검색 결과 목록 컨테이너 크기 수정 - 스켈레톤 표지 크기 및 갯수 수정

--- src/app/search/_components/BookListSkeleton.tsx
+++ src/app/search/_components/BookListSkeleton.tsx
@@ -3,12 +3,13 @@ import Skeleton from "@/app/components/Skeleton";
 export default function BookListSkeleton() {
   return (
     <ul className="w-full max-w-4xl space-y-4">
-      {[...Array(3)].map((_, i) => (
+      {[...Array(6)].map((_, i) => (
         <li key={i} className="flex rounded border p-4">
-          <Skeleton className="mr-4 h-32 w-24" />
+          <Skeleton className="mr-4 h-60 w-40" />
           <div className="flex-1 space-y-2">
             <Skeleton className="h-4 w-1/2" />
             <Skeleton className="h-3 w-3/4" />
+            <Skeleton className="h-3 w-5/6" />
             <Skeleton className="h-3 w-5/6" />
           </div>
         </li>
