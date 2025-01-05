24b95c3ebf96ab9a848801b683d1707d49c59ab4
Choongman Kim <cm730712@gmail.com>
2025-01-05 23:57:07 +0900

    Modify: 검색 페이지 컴포넌트 분리 - 검색 기능 컴포넌트 분리 - 데이터에 번역없을 경우 화면에서 안보이게 처리

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,65 +1,11 @@
-"use client";
-
-import Image from "next/image";
-import { Book } from "@/types/common";
-import Bookmark from "./_components/Bookmark";
 import SearchBar from "./_components/SearchBar";
-import BookListSkeleton from "./_components/BookListSkeleton";
-import { useSearchResultQuery } from "@/hooks/useSearchResult";
+import SearchResult from "./_components/SearchResult";
 
 export default function Search() {
-  const { data, isLoading, isFetching } = useSearchResultQuery();
-
   return (
     <section className="flexCenter mb-10 mt-20 flex-col font-main">
       <SearchBar />
-
-      {isLoading || isFetching ? (
-        <BookListSkeleton />
-      ) : (
-        <ul className="flex flex-col items-center gap-5">
-          {data?.documents.map((el: Book) => {
-            return (
-              <li key={el.isbn} className="searchList">
-                <article className="relative flex justify-center">
-                  <picture className="flexCenter relative mx-8 my-3 w-48 shadow-md">
-                    <Image src={el.thumbnail} fill alt="북커버" />
-                  </picture>
-
-                  <Bookmark />
-                  <div className="border border-border" />
-                  <div className="mx-8 my-4 flex w-8/12 flex-col gap-2">
-                    <h2 className="font-styled text-2xl font-extrabold text-font-textPrimary">
-                      {el.title}
-                    </h2>
-                    <p className="flex-grow text-sm font-medium text-font-textSecondary">
-                      {el.contents}
-                    </p>
-                    <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
-                      <div className="flex">
-                        <dt className="searchDetail">작가</dt>
-                        <dd className="font-semibold">{el.authors}</dd>
-                      </div>
-                      <div className="flex">
-                        <dt className="searchDetail">번역</dt>
-                        <dd className="font-semibold">{el.translators}</dd>
-                      </div>
-                      <div className="flex">
-                        <dt className="searchDetail">출판사</dt>
-                        <dd className="font-semibold">{el.publisher}</dd>
-                      </div>
-                      <div className="flex">
-                        <dt className="searchDetail">출판 날짜</dt>
-                        <dd className="font-semibold">{el.datetime}</dd>
-                      </div>
-                    </dl>
-                  </div>
-                </article>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      <SearchResult />
     </section>
   );
 }
