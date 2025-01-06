8cca9c9d777df2076c875d5bce1e275eab5209a5
Choongman Kim <cm730712@gmail.com>
2025-01-06 09:42:13 +0900

    Modify: api 요청 params 값 변경

--- src/hooks/useSearchResult.ts
+++ src/hooks/useSearchResult.ts
@@ -7,14 +7,14 @@ import { getBookSearch, BookResponse } from "@/libs/apis/searchApi";
 
 export function useSearchResultQuery() {
   const { query } = useSearchResult();
-  const debouncedQuery = useDebounce(query, 300);
+  const debouncedQuery = useDebounce(query, 500);
 
   const { data, isLoading, isError, error, refetch, isFetching } = useQuery<
     BookResponse,
     Error
   >({
     queryKey: ["searchBooks", debouncedQuery],
-    queryFn: () => getBookSearch(debouncedQuery, 1, 10, "accuracy"),
+    queryFn: () => getBookSearch(debouncedQuery, 1, 50, "accuracy"),
     enabled: debouncedQuery.length > 0,
     staleTime: 1000 * 60 * 5,
   });
