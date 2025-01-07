93be813014c2e0517536ab481a79dcee6e10fb8c
Choongman Kim <cm730712@gmail.com>
2025-01-08 03:04:46 +0900

    Modify: 검색 기능 수정 - 실시간 검색 기능 => 검색어 입력 후, Enter or 버튼 클릭으로 변경 - 검색어가 없는 경우, 검색 결과가 없는 경우 상황 UI 구현

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import Image from "next/image";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 import searchBtn from "/public/icons/SearchSearch.svg";
-import { useSearchResult } from "@/stores/searchResult";
 
 export default function SearchBar() {
-  const { query, setQuery } = useSearchResult();
+  const router = useRouter();
+  const [inputValue, setInputValue] = useState("");
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!inputValue.trim()) return;
+    router.push(`/search?query=${encodeURIComponent(inputValue)}`);
   };
   return (
     <form
@@ -22,13 +26,12 @@ export default function SearchBar() {
       </label>
       <input
         type="search"
-        name="search"
-        value={query}
+        value={inputValue}
         id="searchInput"
         placeholder="Search..."
         aria-describedby="search-hint"
+        onChange={(e) => setInputValue(e.target.value)}
         className="grow text-lg text-font-textPrimary outline-none"
-        onChange={(e) => setQuery(e.target.value)}
       />
       <button type="submit" aria-label="검색 버튼" className="search">
         <Image src={searchBtn} width={32} height={32} alt="검색 아이콘" />
