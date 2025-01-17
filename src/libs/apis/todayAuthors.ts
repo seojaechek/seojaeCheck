af0614e9ae5ed913b3b900edc549ef4cd3688bde
shchoon <acckr0604@gmail.com>
2025-01-17 17:03:03 +0900

    Test: revalidateTime test

--- src/libs/apis/todayAuthors.ts
+++ src/libs/apis/todayAuthors.ts
@@ -5,15 +5,10 @@ export const todayAuthors = async (randomIndex: number) => {
   const setTime = new Date();
   setTime.setHours(24, 0, 0);
 
-  console.log("now", now, "setTime", setTime);
-  console.log(Date.parse(now.toString()), Date.parse(setTime.toString()));
-
-  // if (Date.parse(now.toString()) > Date.parse(setTime.toString())) {
-  //   setTime.setHours( 0);
-  // }
+  console.log("now", now.toUTCString(), "setTime", setTime.toUTCString());
 
   const revalidationTime =
-    (Date.parse(setTime.toString()) - Date.parse(now.toString())) / 1000;
+    (Date.parse(setTime.toUTCString()) - Date.parse(now.toUTCString())) / 1000;
 
   console.log(revalidationTime);
 
